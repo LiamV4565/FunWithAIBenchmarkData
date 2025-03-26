ef4d52e36a6ce7c2e1981683bfa8a66b3fcd60cb
matthewboutros <matthew.boutros33@gmail.com>
2025-03-26 14:36:15 -0400

    added class and two modules for tasks with documentation

---
+++ FunWithAIBenchmarkData/visualizationPackage/teamImage.py
@@ -0,0 +1,13 @@
+# File Name : teamImage.py
+# Student Name: Dylan Sams, Liam Vasey, Matthew Boutros
+# email:  
+# Assignment Number: Assignment 08
+# Due Date:   3/27/2025
+# Course #/Section:   IS 4010 001
+# Semester/Year:  Spring 2025
+# Brief Description of the assignment:
+
+# Brief Description of what this module does:
+# Citations: 
+
+# Anything else that's relevant:
