61ab356bbefa7495fe0a4ebcc78c787569eec9ba
Dylan-Sams <samsds@mail.uc.edu>
2025-03-26 21:07:09 -0400

    Added the module team_image() to the file teamImage.py. Using pypi extension pillow.

--- FunWithAIBenchmarkData/visualizationPackage/teamImage.py
+++ FunWithAIBenchmarkData/visualizationPackage/teamImage.py
@@ -1,13 +1,27 @@
 # File Name : teamImage.py
 # Student Name: Dylan Sams, Liam Vasey, Matthew Boutros
-# email:  
+# email:  samsds@mail.uc.edu
 # Assignment Number: Assignment 08
 # Due Date:   3/27/2025
 # Course #/Section:   IS 4010 001
 # Semester/Year:  Spring 2025
-# Brief Description of the assignment:
+# Brief Description of the assignment: This assignment is about displaying an image of our team name and creating a visualization of the data using modularity.
 
-# Brief Description of what this module does:
-# Citations: 
+# Brief Description of what this module does: This module defines the module team_image image and houses the code for said module.
+# Citations: pillow tutorial: https://pillow.readthedocs.io/en/latest/handbook/tutorial.html , where I found it: https://pypi.org/project/pillow/
 
 # Anything else that's relevant:
+
+
+from PIL import Image
+
+def team_image():
+    """
+    opens the image and shows image
+    """
+    image = Image.open("sandslash.jpg")
+    
+    resized = image.resize((200,200))
+    
+    resized.show()
+    
